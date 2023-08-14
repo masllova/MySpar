67cced19a8a2dee48face85b66014aacb35f6b4f
alexa <allexa.msv@gmail.com>
2023-08-14 18:20:29 +0300

    Initial Commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Александра Маслова on 14.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
