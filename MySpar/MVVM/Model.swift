8fd251916622757905d96152c093d910319abaec
alexa <allexa.msv@gmail.com>
2023-08-15 00:51:52 +0300

    fix

--- MySpar/MVVM/Model.swift
+++ MySpar/MVVM/Model.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct ProductCard {
+    var id: String
+    var category: String
+    var inf: String?
+    var image: String
+    var price: String
+    var oldPrice: Int?
+    var measurement: String
+    var discount: Int?
+}
